560c82c19f5e993643bb1ad6f079a6ef84ef7527
GraceWren <Grace.wren@education.nsw.gov.au>
2024-03-30 16:41:21 +1100

    Added intital implementation of multiple choice feilds, checks for correct answers, results page summary at end of quiz with questions correct and incorrect, percentage of questions correct and stop timer

--- eduprod/models.py
+++ eduprod/models.py
@@ -3,8 +3,14 @@ from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    answer_text = models.TextField()
+    question_text = models.TextField(max_length=1000)
+    answertextA = models.CharField(max_length = 50, default="", blank=True)  #Optional. Required for multiple choice only
+    answertextB = models.CharField(max_length = 50, default="", blank=True)  #Optional
+    answertextC = models.CharField(max_length = 50, default="", blank=True)  #Optional
+    answertextD = models.CharField(max_length = 50, default="", blank=True)  #Optional
+    correctanswer = models.CharField(max_length = 50, default="" ) #Mandatory. Answer required for all questions
+    hasmultiplechoice = models.BooleanField(default=True) #True if multiple choice question. False for single answer
+    question_image = models.ImageField(default="", blank=True, upload_to="eduprod/static") 
 
     def __str__(self):
         return self.question_text
--- eduprod/static/eduprod/quiz.js
+++ eduprod/static/eduprod/quiz.js
@@ -1,24 +1,128 @@
+//Global variables
+var questions = JSON.parse(document.getElementById('content').getAttribute('data-questions'));
+var currentQuestionIndex = 0;
+var answerArray = []; //Boolean array. T = Answer correct, F = Wrong
+for (let i = 0; i < questions.length; i += 1) { //Initiallly set to false
+    answerArray.push(false);
+}
+//Check user answer is correct
+function CheckUserAnswer() {
+    let uansewer = document.forms["UserAnswerForm"]["UserAnswer"].value;
+    //const questions = JSON.parse(document.getElementById('content').getAttribute('data-questions'));
+    let correctanswer = questions[currentQuestionIndex].fields.correctanswer;
+    //alert ("User answer: " + uansewer);
+    //alert ("Correct answer: " + correctanswer);
+    //alert ("Current Q number: " + currentQuestionIndex);
+    if (uansewer == correctanswer) {
+        alert("Answer is correct");
+        answerArray[currentQuestionIndex] = true;
+        return false;
+    }
+    else {
+        alert("Answer is wrong. Correct answer is: " + correctanswer);
+        answerArray[currentQuestionIndex] = false;
+        return false;       
+    }
+}  
+
+//Format the min and sec values to have a leading zero when required. e.g. 9 => 09
+function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+        return "0" + valString;
+    } else {
+        return valString;
+    }
+}
+
 
 document.addEventListener("DOMContentLoaded", function() {
-    let currentQuestionIndex = 0;
-    const questions = JSON.parse(document.getElementById('content').getAttribute('data-questions'));
+ 
+    var questions = JSON.parse(document.getElementById('content').getAttribute('data-questions'));
     const content = document.getElementById('content');
     const btn = document.getElementById('revealBtn');
 
+    //Display current question number
+    var CurrentQ = document.getElementById("CurrentQ");
+    var MaxQ = document.getElementById("MaxQ");
+    MaxQ.innerHTML = questions.length
+
+    //Setup timer variables to count seconds
+    var minutesLabel = document.getElementById("minutes");
+    var secondsLabel = document.getElementById("seconds");
+    var totalSeconds = 0;
+    timerId = setInterval(setTime, 1000);
+
+    //Function called once per second to update the timer display
+    function setTime() {
+        ++totalSeconds;
+        secondsLabel.innerHTML = pad(totalSeconds % 60);
+        minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+    }
+    
     function displayQuestion() {
         if (currentQuestionIndex < questions.length) {
-            const question = questions[currentQuestionIndex].fields.question_text;
-            const answer = questions[currentQuestionIndex].fields.answer_text;
-            content.innerHTML = `<div class='question'>Question: ${question}</div><div class='answer' style='display: none;'>Answer: ${answer}</div>`;
+            //Get the fields from the database for the current question
+            let question = questions[currentQuestionIndex].fields.question_text;
+            let correctanswer = questions[currentQuestionIndex].fields.correctanswer;
+            let answertextA = questions[currentQuestionIndex].fields.answertextA;
+            let answertextB = questions[currentQuestionIndex].fields.answertextB;
+            let answertextC = questions[currentQuestionIndex].fields.answertextC;
+            let answertextD = questions[currentQuestionIndex].fields.answertextD;
+            let question_image = questions[currentQuestionIndex].fields.question_image;
+            let hasmultiplechoice = questions[currentQuestionIndex].fields.hasmultiplechoice;
+            //Build up the HTML code to display the question. Note the use of Backticks/Grave Accents to define the strings.
+            HTMLQuestion = "";
+            //if there is an image for this question, then display it before the question.
+            if (question_image.length > 0) {
+                HTMLQuestion += `<img src="../${question_image}">\n`;
+            }
+            HTMLQuestion += `<div class='question'>Question: ${question}</div>`;
+            if (hasmultiplechoice) {
+                HTMLQuestion += `<div class='question'>A): ${answertextA}</div>`;
+                HTMLQuestion += `<div class='question'>B): ${answertextB}</div>`;
+                HTMLQuestion += `<div class='question'>C): ${answertextC}</div>`;
+                HTMLQuestion += `<div class='question'>D): ${answertextD}</div>`;
+            }
+            //Add form for input to get users answer
+            HTMLQuestion += `<form name="UserAnswerForm" onsubmit="return CheckUserAnswer()">`;
+            HTMLQuestion += `<label for="UserAnswer">Answer:</label><br>`;
+            HTMLQuestion += `<input type="text" id="UserAnswer" name="UserAnswer">`
+            HTMLQuestion += `<input type="submit" value="Submit"></form><br>`;
+
+            HTMLQuestion += `<div class='answer' style='display: none;'> Answer: ${correctanswer}</div>\n`;
+            content.innerHTML = HTMLQuestion;
             btn.textContent = "Reveal Answer";
-        } else {
-            content.innerHTML = "No more questions.";
+            CurrentQ.innerHTML = currentQuestionIndex+1
+        } else {// show results at end of quiz
+            clearInterval(timerId); 
+            ResultSummary = "<h1>Results</h1>"
+            QuestionsCorrect = 0
+            QuestionsIncorrect = 0
+            for (let i = 0; i < questions.length; i += 1) { //checking the value of the answer array to display which questions are correct
+                DisplayNum = i+1;//i (index) + 1 is used becuase of zero index
+                if (answerArray[i] == true) {
+                     ResultSummary += "Question " + DisplayNum + ": correct<br>"
+                     QuestionsCorrect += 1;
+                }
+                else {
+                    ResultSummary += "Question " + DisplayNum + ": incorrect<br>";
+                    QuestionsIncorrect += 1;
+                }
+            }
+            PercentageCorrect = QuestionsCorrect/questions.length*100;
+            ResultSummary += "<br><br> Percentage Correct: " + PercentageCorrect + "%";
+            content.innerHTML = ResultSummary;
+
+            
+        
             btn.style.display = "none";
         }
     }
 
     displayQuestion();
 
+    //Function called when Next Question or reveal answer button is clicked.
     btn.addEventListener("click", function() {
         const answerElement = content.querySelector('.answer');
         if (btn.textContent === "Reveal Answer") {
